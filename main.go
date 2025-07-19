e819568145974f4da1b6a8255f687871daebc51c
kenshinapa <r2padillaayala@gmail.com>
2025-07-19 02:02:48 -0600

    2 proposal 1

--- main.go
+++ main.go
@@ -3,9 +3,49 @@ package main
 import (
 	"fmt"
 
-	sum "github.com/kenshinapa/LetsCodeLeetCode/two-sum"
+	add "github.com/kenshinapa/LetsCodeLeetCode/add-two-numbers"
 )
 
 func main() {
-	fmt.Println(sum.TwoSum([]int{7, 11, 15, 2}, 9)) // Output: [0, 3]
+	result := add.AddTwoNumbers(&add.ListNode{
+		Val: 9,
+		Next: &add.ListNode{
+			Val: 9,
+			Next: &add.ListNode{
+				Val: 9,
+				Next: &add.ListNode{
+					Val: 9,
+				},
+			},
+		},
+	}, &add.ListNode{
+		Val: 9,
+		Next: &add.ListNode{
+			Val: 9,
+			Next: &add.ListNode{
+				Val: 9,
+				Next: &add.ListNode{
+					Val: 9,
+				},
+			},
+		},
+	}) // Output: 8 -> 9 -> 9 -> 9 -> 1
+
+	fmt.Println("Result of AddTwoNumbers:")
+
+	// Print first value
+	fmt.Printf("%d", result.Val)
+	if result.Next != nil {
+		fmt.Print(" -> ")
+	}
+
+	n := result.Next
+
+	for n != nil {
+		fmt.Print(n.Val)
+		if n.Next != nil {
+			fmt.Print(" -> ")
+		}
+		n = n.Next
+	}
 }
