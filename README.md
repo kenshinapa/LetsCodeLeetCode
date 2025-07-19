0f61a852a239d7e18e564a1b5bc61c158ed333d6
kenshinapa <r2padillaayala@gmail.com>
2025-07-18 23:53:45 -0600

    new problem, readme note

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LetsCodeLeetCode
 
 Just created a new account on LeetCode and made this repo to track my progress.
+Using Golang.
---
+++ find-unique-binary-string/find_unique_binary_string.go
@@ -0,0 +1,9 @@
+// Package finduniquebinarystring - Problem 1980. Find Unique Binary String
+package finduniquebinarystring
+
+// FindDifferentBinaryString
+// Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+// Note: exporting function to import in main.go. In Platform, the function name is unexported.
+func FindDifferentBinaryString(nums []string) string {
+
+}
