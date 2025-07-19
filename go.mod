2d95668ad156a193f277fd5093b8fcbf991c224c
kenshinapa <r2padillaayala@gmail.com>
2025-07-18 23:48:36 -0600

    more input

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kenshinapa/LetsCodeLeetCode
+
+go 1.24.4
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	rs "github.com/kenshinapa/LetsCodeLeetCode/remove-subdirectories"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	data := []string{
+		"/a/b",
+		"/a",
+		"/a/b/c",
+		"/ax/b",
+		"/c/d",
+		"/c/d/e",
+		"/c",
+		"/c/f",
+		"/d/d/d",
+	}
+	result := rs.RemoveSubfolders(data)
+
+	fmt.Println("Result:", result)
 }
